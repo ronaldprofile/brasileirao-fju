e3a7bb2f47f7db87af377e1d2503d6294e655dfc
Ronald Tomaz <ronaldtmprofile@gmail.com>
2023-04-28 15:33:47 -0300

    fix: eslint

--- src/schemas/player.ts
+++ src/schemas/player.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod'
 
-const MAX_FILE_SIZE = 5 * 1024 * 1024 // 5mb
-const ACCEPTED_IMAGE_TYPES = [
-  'image/jpeg',
-  'image/jpg',
-  'image/png',
-  'image/webp',
-]
+// const MAX_FILE_SIZE = 5 * 1024 * 1024 // 5mb
+// const ACCEPTED_IMAGE_TYPES = [
+//   'image/jpeg',
+//   'image/jpg',
+//   'image/png',
+//   'image/webp',
+// ]
 
 export const createPlayerFormSchema = z.object({
   name: z
