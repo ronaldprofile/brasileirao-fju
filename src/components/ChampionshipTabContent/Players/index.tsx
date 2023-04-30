154fa1998100805fa71e975864e0c18dfd83a92d
Ronald Tomaz <ronaldtmprofile@gmail.com>
2023-04-30 11:11:27 -0300

    add state loading

--- src/components/ChampionshipTabContent/Players/index.tsx
+++ src/components/ChampionshipTabContent/Players/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { api } from '@/lib/axios'
-import { Shield } from '@phosphor-icons/react'
+import { CircleNotch, Shield } from '@phosphor-icons/react'
+import cx from 'clsx'
 
 interface Player {
   id: string
@@ -12,44 +13,62 @@ interface Player {
 
 export function Players() {
   const [players, setPlayers] = useState<Player[]>([])
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     async function getPlayers() {
+      setIsLoading(true)
+
       const response = await api.get<{ data: Player[] }>('players')
 
       const { data: allPlayers } = response.data
       setPlayers(allPlayers)
+
+      setIsLoading(false)
     }
 
     getPlayers()
   }, [])
 
   return (
-    <div className="bg-[#202024]">
-      <div className="grid grid-cols-3 sm:grid-cols-7 gap-2 px-2 pt-2 pb-10">
-        {players.map((player) => {
-          return (
-            <div key={player.id} className="border border-[#323238] rounded-md">
-              <img
-                className="w-full rounded-t"
-                src={player.avatar}
-                alt={player.name}
-              />
+    <div
+      className={cx('bg-[#202024]', {
+        'flex justify-center items-center': isLoading,
+      })}
+    >
+      {isLoading ? (
+        <div className="p-2  text-white">
+          <CircleNotch size={24} className="animate-spin" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-3 sm:grid-cols-7 gap-2 px-2 pt-2 pb-10">
+          {players.map((player) => {
+            return (
+              <div
+                key={player.id}
+                className="flex flex-col border border-[#323238] rounded-md"
+              >
+                <img
+                  className="w-full rounded-t h-[100px] object-cover"
+                  src={player.avatar}
+                  alt={player.name}
+                />
 
-              <div className="p-2 flex flex-col gap-1 text-[#a9a9b2]">
-                <span className="block max-w-[58px] text-sm overflow-hidden text-ellipsis">
-                  {player.name}
-                </span>
+                <div className="p-2 flex flex-1 flex-col justify-end gap-1 text-[#a9a9b2]">
+                  <span className="block max-w-[58px] text-sm overflow-hidden text-ellipsis">
+                    {player.name}
+                  </span>
 
-                <span className="flex items-center gap-2 text-xs">
-                  <Shield size={16} />
-                  FJU
-                </span>
+                  <span className="flex items-center gap-2 text-xs">
+                    <Shield size={16} />
+                    FJU
+                  </span>
+                </div>
               </div>
-            </div>
-          )
-        })}
-      </div>
+            )
+          })}
+        </div>
+      )}
     </div>
   )
 }
