32ff50634701226e1dab75e20e32d72fd4fde997
Ronald Tomaz <ronaldtmprofile@gmail.com>
2023-04-30 12:26:31 -0300

    environment variables

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
 export const api = axios.create({
-  baseURL: 'https://fju-api.vercel.app/api',
+  baseURL: API_URL,
 })
