904803da3ef4db2a6ef07807ad1c77140e33f026
Ronald Tomaz <ronaldtmprofile@gmail.com>
2023-05-11 09:54:22 -0300

    fixed header

--- src/components/ChampionshipLayout/index.tsx
+++ src/components/ChampionshipLayout/index.tsx
@@ -8,15 +8,17 @@ interface ChampionshipLayoutProps {
 export function ChampionshipLayout({ children }: ChampionshipLayoutProps) {
   return (
     <div className="w-full">
-      <header className="flex flex-col bg-[#202024] border-b border-b-[#323238]">
-        <div className="p-6">
-          <strong className="text-white">Brasileirão</strong>
-        </div>
+      <div className="fixed top-0 left-0 z-50 w-full">
+        <header className="flex flex-col bg-[#202024] border-b border-b-[#323238]">
+          <div className="p-6">
+            <strong className="text-white">Brasileirão</strong>
+          </div>
 
-        <ChampionshipTabs />
-      </header>
+          <ChampionshipTabs />
+        </header>
+      </div>
 
-      <main className="mt-6">
+      <main className="mt-32">
         <div className="w-full max-w-3xl h-12 mx-auto px-6 lg:px-0">
           {children}
         </div>
