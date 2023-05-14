3d63ff5c72573aad25a02164034a1d14c9c0bf6d
Ronald Tomaz <ronaldtmprofile@gmail.com>
2023-05-14 12:28:42 -0300

    fix: loading skeleton matches

---
+++ src/components/MatchCard/MatchSkeleton.tsx
@@ -0,0 +1,24 @@
+import Skeleton from 'react-loading-skeleton'
+
+export function MatchCardSkeleton() {
+  return (
+    <div className="h-32 px-6 py-4 bg-[#202024] border border-[#323238] flex items-center justify-between cursor-pointer hover:bg-[#323238]/60 transition-colors">
+      <div className="flex flex-col gap-1">
+        <div className="flex items-center gap-3">
+          <Skeleton circle width={24} height={24} />
+          <Skeleton width={100} height={22} />
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Skeleton circle width={24} height={24} />
+          <Skeleton width={100} height={22} />
+        </div>
+      </div>
+
+      <div className="h-full pl-4 flex flex-col items-center justify-center border-l-[1.2px] border-l-[#323238]">
+        <Skeleton width={40} />
+        <Skeleton width={30} />
+      </div>
+    </div>
+  )
+}
--- src/pages/championship/index.tsx
+++ src/pages/championship/index.tsx
@@ -4,19 +4,23 @@ import { MatchCard } from '@/components/MatchCard'
 import { useGetMatches } from '@/hooks/get-matches-championship'
 import { useRoundsChampionship } from '@/hooks/get-rounds-championship'
 import { ArrowLeft, ArrowRight } from '@phosphor-icons/react'
+import Skeleton from 'react-loading-skeleton'
+import { MatchCardSkeleton } from '@/components/MatchCard/MatchSkeleton'
 
 const INITIAL_ROUND = { name: '', uuid: '' }
 
 export default function Championship() {
   const [currentRound, setCurrentRound] = useState(0)
 
-  const { data: roundsList } = useRoundsChampionship()
+  const { data: roundsList, isLoading: isLoadingRound } =
+    useRoundsChampionship()
 
   const rounds = roundsList?.rounds ?? []
   const roundsTotal = rounds?.length ?? 0
   const round = rounds[currentRound] ?? INITIAL_ROUND
 
-  const { data } = useGetMatches(round?.uuid)
+  const { data, isLoading: isLoadingMatches } = useGetMatches(round?.uuid)
+  const matchesTotal = data?.matches.length ?? 4
 
   function handleNextRound() {
     if (currentRound < roundsTotal - 1) {
@@ -35,32 +39,54 @@ export default function Championship() {
       <ChampionshipLayout>
         <div>
           <div className="pt-3 px-4 pb-2 flex items-center justify-between bg-[#202024] border-b border-b-[#323238]">
-            <button
-              onClick={handlePreviousRound}
-              className="p-2 hover:border-none hover:bg-[#323238] transition-all flex items-center gap-2 border border-[#323338] rounded-md"
-            >
-              <ArrowLeft size={20} />
-              <span className="text-sm text-[#a9a9b2]">Rodada anterior</span>
-            </button>
+            {isLoadingRound ? (
+              <>
+                <Skeleton width={100} height={38} />
+                <Skeleton width={100} height={26} />
+                <Skeleton width={100} height={38} />
+              </>
+            ) : (
+              <>
+                <button
+                  onClick={handlePreviousRound}
+                  className="p-2 hover:border-none hover:bg-[#323238] transition-all flex items-center gap-2 border border-[#323338] rounded-md"
+                >
+                  <ArrowLeft size={20} />
+                  <span className="text-sm text-[#a9a9b2]">
+                    Rodada anterior
+                  </span>
+                </button>
 
-            <span className="text-sm text-[#A9A9B2]">
-              {round.name} de {roundsTotal}
-            </span>
+                <span className="text-sm text-[#A9A9B2]">
+                  {round.name} de {roundsTotal}
+                </span>
 
-            <button
-              onClick={handleNextRound}
-              className="p-2 hover:border-none hover:bg-[#323238] transition-all flex items-center gap-2 border border-[#323338] rounded-md"
-            >
-              <span className="text-sm text-[#a9a9b2]">Próxima rodada</span>
-              <ArrowRight size={20} />
-            </button>
+                <button
+                  onClick={handleNextRound}
+                  className="p-2 hover:border-none hover:bg-[#323238] transition-all flex items-center gap-2 border border-[#323338] rounded-md"
+                >
+                  <span className="text-sm text-[#a9a9b2]">Próxima rodada</span>
+                  <ArrowRight size={20} />
+                </button>
+              </>
+            )}
           </div>
 
           <div className="mt-6">
-            <div className="grid grid-cols-2 divide-x divide-y divide-[#323238]">
-              {data?.matches.map((match) => (
-                <MatchCard key={match.uuid} match={match} />
-              ))}
+            <div className="grid grid-cols-2 gap-1">
+              {isLoadingMatches ? (
+                <>
+                  {Array.from({ length: matchesTotal }).map((_, index) => {
+                    return <MatchCardSkeleton key={index} />
+                  })}
+                </>
+              ) : (
+                <>
+                  {data?.matches.map((match) => (
+                    <MatchCard key={match.uuid} match={match} />
+                  ))}
+                </>
+              )}
             </div>
           </div>
         </div>
