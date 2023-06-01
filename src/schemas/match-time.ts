d2af6cd584bcf2097d7f06a728cffce39f1b7428
Ronald Tomaz <ronaldtmprofile@gmail.com>
2023-06-01 13:11:13 -0300

    fix: validation hour and minute

--- src/schemas/match-time.ts
+++ src/schemas/match-time.ts
@@ -4,15 +4,34 @@ export const createConfrontationDateSchema = z.object({
   hour: z
     .string()
     .min(2, { message: 'Preencha o campo com 2 dígitos' })
-    .max(2)
+    .max(2, { message: 'Preencha o campo com 2 dígitos' })
     .nonempty('Preencha esse campo')
-    .transform((str) => Number(str)),
+    .regex(/^\d{2}$/, {
+      message: 'A hora deve ser um valor numérico de dois dígitos',
+    })
+    .refine(
+      (value) => {
+        const hour = Number(value)
+        return hour >= 0 && hour <= 23
+      },
+      { message: 'A hora deve estar entre 0 e 23' },
+    ),
+
   minute: z
     .string()
     .min(2, { message: 'Preencha o campo com 2 dígitos' })
-    .max(2)
+    .max(2, { message: 'Preencha o campo com 2 dígitos' })
     .nonempty()
-    .transform((str) => Number(str)),
+    .regex(/^\d{2}$/, {
+      message: 'Os minutos devem ser um valor numérico de dois dígitos',
+    })
+    .refine(
+      (value) => {
+        const minute = Number(value)
+        return minute >= 0 && minute <= 59
+      },
+      { message: 'Os minutos devem estar entre 0 e 59' },
+    ),
 })
 
 export type createConfrontationDateFormData = z.infer<
